7f984e3e88192158320a8f12c59a81dd7f97ced3
Terry Sigle <terry@sigle.com>
2021-04-19 15:58:18 -0500

    updated help and remove list

--- pi-test.sh
+++ pi-test.sh
@@ -11,9 +11,9 @@ pi()
 ./pi ${@}
 }
 
-pi pingone list users
-pi pingone list groups
-pi pingone list populations
+pi pingone get users
+pi pingone get groups
+pi pingone get populations
 
 pi pingone add population pi-test-pop
 pi pingone add group pi-test-group -p pi-test-pop
@@ -21,9 +21,9 @@ pi pingone add user pi-test-user@_example.com pi-test-First pi-test-Last -p pi-t
 
 pi pingone add-user-group pi-test-user@_example.com pi-test-group
 
-pi pingone get population pi-test-pop
-pi pingone get user pi-test-user@_example.com
-pi pingone get group pi-test-group
+pi pingone get populations -n pi-test-pop
+pi pingone get users -n pi-test-user@_example.com
+pi pingone get groups -n pi-test-group
 
 pi pingone delete-user-group pi-test-user@_example.com pi-test-group
 pi pingone delete user pi-test-user@_example.com

903a74c132b9df9c52218fca4a04465a8633e9cb
Terry Sigle <terry@sigle.com>
2021-04-20 12:06:03 -0500

    transition to pingctl

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ########################################################################################################################
 #
-# This script is used to Install a Ping Identity pi CLI
+# This script is used to Install pingctl, a Ping Identity CLI
 #
 # ------------
-# Installs pi               into     .
+# Installs pingctl               into     .
 # ------------
 
 INSTALL_DIR=$(pwd)
 
-if [ -f "$INSTALL_DIR/pi" ]; then
-  echo "pi already installed in ${INSTALL_DIR}"
+if [ -f "$INSTALL_DIR/pingctl" ]; then
+  echo "pingctl already installed in ${INSTALL_DIR}"
   echo "Please remove or move to reinstall"
   exit 1
 fi
@@ -29,42 +29,42 @@ trap cleanup EXIT
 
 cd "$TMP_DIR" >& /dev/null || echo "Unable to chanage to temporary directory" || exit 1
 
-curl -s https://raw.githubusercontent.com/pingidentity/homebrew-devops/master/Formula/pi.rb |\
+curl -s https://raw.githubusercontent.com/pingidentity/homebrew-devops/master/Formula/pingctl.rb |\
   grep url |\
   cut -d '"' -f 2 |\
   xargs curl -s -O -L
 
 tar xzf ./*.tar.gz
 
-cd pi-* || echo "Unable to chanage to pi-*" || exit 1
+cd pingctl-* || echo "Unable to chanage to pingctl-*" || exit 1
 
-cp pi                  "$INSTALL_DIR/."
+cp pingctl                  "$INSTALL_DIR/."
 
 echo "
 ################################################################################
-                     Welcome to Ping Identity pi CLI!
+                     Welcome to Ping Identity pingctl CLI!
 
   You have just downloaded:
 
-    ${INSTALL_DIR}/pi
+    ${INSTALL_DIR}/pingctl
 
   It is recommended to:
-    1. copy your 'pi' to a location in your PATH (i.e. ~/bin or /usr/local/bin)
+    1. copy your 'pingctl' to a location in your PATH (i.e. ~/bin or /usr/local/bin)
     2. Recommended additional utilities:
-          base64 (used by pi)
+          base64 (used by pingctl)
           docker
           docker-compose
           envsubst
           helm
-          jq (used by pi)
-          jwt (used by pi)
+          jq (used by pingctl)
+          jwt (used by pingctl)
           k9s
           kubectl
           kubectx (includes kubens)
-          openssl (used by pi)
+          openssl (used by pingctl)
 
   Example:
-    sudo mv ${INSTALL_DIR}/pi /usr/local/bin/.
-    pi config
+    sudo mv ${INSTALL_DIR}/pingctl /usr/local/bin/.
+    pingctl config
 ################################################################################
 "

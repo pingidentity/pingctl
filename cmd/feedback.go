65f652d7c5f9cd50c2dbc2efdd4754be4f51ba56
Erik Ostien <erikostien@pingidentity.com>
2024-01-23 14:01:11 -0700

    Satisfy linters, and remove unneeded linters in config for golangci-lint

--- cmd/feedback.go
+++ cmd/feedback.go
@@ -16,8 +16,6 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -32,7 +30,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("feedback called")
 	},
 }
 

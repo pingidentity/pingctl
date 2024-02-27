357d4bd26630ac8c293913d5af79659840d93c3f
Erik Ostien <erikostien@pingidentity.com>
2024-02-27 08:37:32 -0700

    Remove API call for default theme resource

--- internal/connector/pingone_platform/resources/pingone_branding_theme_default.go
+++ internal/connector/pingone_platform/resources/pingone_branding_theme_default.go
@@ -1,8 +1,6 @@
 package resources
 
 import (
-	"fmt"
-
 	"github.com/pingidentity/pingctl/internal/connector"
 	"github.com/pingidentity/pingctl/internal/logger"
 )
@@ -28,48 +26,13 @@ func (r *PingoneBrandingThemeDefaultResource) ExportAll() (*[]connector.ImportBl
 
 	l.Debug().Msgf("Fetching all pingone_branding_theme_default resources...")
 
-	entityArray, response, err := r.clientInfo.ApiClient.ManagementAPIClient.BrandingThemesApi.ReadBrandingThemes(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID).Execute()
-	defer response.Body.Close()
-	if err != nil {
-		l.Error().Err(err).Msgf("ReadBrandingSettings Response Code: %s\nResponse Body: %s", response.Status, response.Body)
-		return nil, err
-	}
-
-	if entityArray == nil {
-		l.Error().Msgf("Returned ReadBrandingThemes() entity array is nil.")
-		l.Error().Msgf("ReadBrandingThemes Response Code: %s\nResponse Body: %s", response.Status, response.Body)
-		return nil, fmt.Errorf("failed to fetch pingone_branding_theme_default resources via ReadBrandingThemes()")
-	}
-
-	embedded, embeddedOk := entityArray.GetEmbeddedOk()
-	if !embeddedOk {
-		l.Error().Msgf("Returned ReadBrandingThemes() embedded data is nil.")
-		l.Error().Msgf("ReadBrandingThemes Response Code: %s\nResponse Body: %s", response.Status, response.Body)
-		return nil, fmt.Errorf("failed to fetch pingone_branding_theme_default resources via ReadBrandingThemes()")
-	}
-
 	importBlocks := []connector.ImportBlock{}
 
-	for _, theme := range embedded.GetThemes() {
-		// Only add an import block for the default/active theme
-		themeDefault, themeDefaultOk := theme.GetDefaultOk()
-		if themeDefaultOk && *themeDefault {
-			themeConfiguration, themeConfigurationOk := theme.GetConfigurationOk()
-			var themeName *string
-			var themeNameOk = false
-			if themeConfigurationOk {
-				themeName, themeNameOk = themeConfiguration.GetNameOk()
-			}
-
-			if themeConfigurationOk && themeNameOk {
-				importBlocks = append(importBlocks, connector.ImportBlock{
-					ResourceType: r.ResourceType(),
-					ResourceName: fmt.Sprintf("%s_active", *themeName),
-					ResourceID:   r.clientInfo.ExportEnvironmentID,
-				})
-			}
-		}
-	}
+	importBlocks = append(importBlocks, connector.ImportBlock{
+		ResourceType: r.ResourceType(),
+		ResourceName: "active_theme",
+		ResourceID:   r.clientInfo.ExportEnvironmentID,
+	})
 
 	return &importBlocks, nil
 }

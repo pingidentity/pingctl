6f83357cf1a610fce84ac91a9522e0eb926e6810
Erik Ostien <erikostien@pingidentity.com>
2024-02-27 13:46:24 -0700

    Retain error messages on failed tests

--- cmd/auth/auth_test.go
+++ cmd/auth/auth_test.go
@@ -21,13 +21,13 @@ func TestAuthLoginCmd_Execute(t *testing.T) {
 	rootCmd.SetArgs([]string{"auth", "login"})
 
 	// Execute the command
-	err := rootCmd.Execute()
-	if err != nil {
+	executeErr := rootCmd.Execute()
+	if executeErr != nil {
 		logContent, err := os.ReadFile(os.Getenv("PINGCTL_LOG_PATH"))
 		if err == nil {
 			t.Logf("Captured Logs: %s", string(logContent[:]))
 		}
-		t.Fatalf("Err: %q, Captured StdOut: %q", err, stdout.String())
+		t.Fatalf("Err: %q, Captured StdOut: %q", executeErr, stdout.String())
 	}
 }
 

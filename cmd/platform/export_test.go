3c6e0033875e95e48fb13b9f7c6d64a52e6ca44b
Erik Ostien <erikostien@pingidentity.com>
2024-02-27 13:25:30 -0700

    Use is.TempDir for testing stdout

--- cmd/platform/export_test.go
+++ cmd/platform/export_test.go
@@ -18,15 +18,15 @@ func TestPlatformExportCmd_Execute(t *testing.T) {
 	rootCmd.SetOut(&stdout)
 	rootCmd.SetErr(&stdout)
 
-	rootCmd.SetArgs([]string{"platform", "export", "--output-directory", os.Getenv("TMPDIR"), "--overwrite"})
+	rootCmd.SetArgs([]string{"platform", "export", "--output-directory", os.TempDir(), "--overwrite"})
 
 	// Execute the command
 	executeErr := rootCmd.Execute()
 	if executeErr != nil {
 		logContent, err := os.ReadFile(os.Getenv("PINGCTL_LOG_PATH"))
 		if err == nil {
-			t.Logf("Captured Logs: %q", string(logContent[:]))
+			t.Logf("Captured Logs: %s", string(logContent[:]))
 		}
-		t.Fatalf("Export Command failed. Make sure to have PingOne env variables set if test is failing.\nErr: %q, Captured StdOut: %q", executeErr, stdout.String())
+		t.Fatalf("Export Command failed. Make sure to have PingOne env variables set if test is failing.\nErr: %q, Captured StdOut: %s", executeErr, stdout.String())
 	}
 }

78008ef849077e3394c7673d69bd99438cae5b0a
erikostien-pingidentity <69643860+erikostien-pingidentity@users.noreply.github.com>
2024-02-27 09:05:38 -0700

    PDI-1513: Add pingone_branding_theme_default (Resource) Export (#46)
    * PDI-1513: Add pingone_branding_theme_default (Resource) Export
    
    * Remove API call for default theme resource

---
+++ internal/connector/pingone_platform/resources/pingone_branding_theme_default.go
@@ -0,0 +1,42 @@
+package resources
+
+import (
+	"github.com/pingidentity/pingctl/internal/connector"
+	"github.com/pingidentity/pingctl/internal/logger"
+)
+
+// Verify that the resource satisfies the exportable resource interface
+var (
+	_ connector.ExportableResource = &PingoneBrandingThemeDefaultResource{}
+)
+
+type PingoneBrandingThemeDefaultResource struct {
+	clientInfo *connector.SDKClientInfo
+}
+
+// Utility method for creating a PingoneBrandingThemeDefaultResource
+func BrandingThemeDefaultResource(clientInfo *connector.SDKClientInfo) *PingoneBrandingThemeDefaultResource {
+	return &PingoneBrandingThemeDefaultResource{
+		clientInfo: clientInfo,
+	}
+}
+
+func (r *PingoneBrandingThemeDefaultResource) ExportAll() (*[]connector.ImportBlock, error) {
+	l := logger.Get()
+
+	l.Debug().Msgf("Fetching all pingone_branding_theme_default resources...")
+
+	importBlocks := []connector.ImportBlock{}
+
+	importBlocks = append(importBlocks, connector.ImportBlock{
+		ResourceType: r.ResourceType(),
+		ResourceName: "active_theme",
+		ResourceID:   r.clientInfo.ExportEnvironmentID,
+	})
+
+	return &importBlocks, nil
+}
+
+func (r *PingoneBrandingThemeDefaultResource) ResourceType() string {
+	return "pingone_branding_theme_default"
+}

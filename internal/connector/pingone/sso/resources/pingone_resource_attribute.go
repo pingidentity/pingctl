deb49ca2b5de0d069bab4f79a516c56ef6c6d243
erikostien-pingidentity <69643860+erikostien-pingidentity@users.noreply.github.com>
2024-03-21 10:04:32 -0600

    PDI-1557: Add pingone_resource_attribute (Resource) Export (#99)

---
+++ internal/connector/pingone/sso/resources/pingone_resource_attribute.go
@@ -0,0 +1,81 @@
+package resources
+
+import (
+	"fmt"
+
+	"github.com/pingidentity/pingctl/internal/connector"
+	"github.com/pingidentity/pingctl/internal/connector/common"
+	"github.com/pingidentity/pingctl/internal/logger"
+)
+
+// Verify that the resource satisfies the exportable resource interface
+var (
+	_ connector.ExportableResource = &PingoneResourceAttributeResource{}
+)
+
+type PingoneResourceAttributeResource struct {
+	clientInfo *connector.SDKClientInfo
+}
+
+// Utility method for creating a PingoneResourceAttributeResource
+func ResourceAttribute(clientInfo *connector.SDKClientInfo) *PingoneResourceAttributeResource {
+	return &PingoneResourceAttributeResource{
+		clientInfo: clientInfo,
+	}
+}
+
+func (r *PingoneResourceAttributeResource) ExportAll() (*[]connector.ImportBlock, error) {
+	l := logger.Get()
+
+	l.Debug().Msgf("Fetching all %s resources...", r.ResourceType())
+
+	apiExecuteFunc := r.clientInfo.ApiClient.ManagementAPIClient.ResourcesApi.ReadAllResources(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID).Execute
+	apiFunctionName := "ReadAllResources"
+
+	embedded, err := common.GetManagementEmbedded(apiExecuteFunc, apiFunctionName, r.ResourceType())
+	if err != nil {
+		return nil, err
+	}
+
+	importBlocks := []connector.ImportBlock{}
+
+	l.Debug().Msgf("Generating Import Blocks for all %s resources...", r.ResourceType())
+
+	for _, resource := range embedded.GetResources() {
+		resourceId, resourceIdOk := resource.GetIdOk()
+		resourceName, resourceNameOk := resource.GetNameOk()
+
+		if resourceIdOk && resourceNameOk {
+			apiExecuteFunc = r.clientInfo.ApiClient.ManagementAPIClient.ResourceAttributesApi.ReadAllResourceAttributes(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID, *resourceId).Execute
+			apiFunctionName = "ReadAllResourceAttributes"
+
+			embedded, err = common.GetManagementEmbedded(apiExecuteFunc, apiFunctionName, r.ResourceType())
+			if err != nil {
+				return nil, err
+			}
+
+			for _, resourceAttribute := range embedded.GetAttributes() {
+				if resourceAttribute.ResourceAttribute == nil {
+					continue
+				}
+
+				resourceAttributeId, resourceAttributeIdOk := resourceAttribute.ResourceAttribute.GetIdOk()
+				resourceAttributeName, resourceAttributeNameOk := resourceAttribute.ResourceAttribute.GetNameOk()
+
+				if resourceAttributeIdOk && resourceAttributeNameOk {
+					importBlocks = append(importBlocks, connector.ImportBlock{
+						ResourceType: r.ResourceType(),
+						ResourceName: fmt.Sprintf("%s_%s", *resourceName, *resourceAttributeName),
+						ResourceID:   fmt.Sprintf("%s/%s/%s", r.clientInfo.ExportEnvironmentID, *resourceId, *resourceAttributeId),
+					})
+				}
+			}
+		}
+	}
+
+	return &importBlocks, nil
+}
+
+func (r *PingoneResourceAttributeResource) ResourceType() string {
+	return "pingone_resource_attribute"
+}

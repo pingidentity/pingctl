c8fc7b67bb5eb4886b8f23be708713ed5d32b016
Patrick Cowland <patrickcowland@pingidentity.com>
2024-03-26 15:00:45 +0000

    Add Google feedback form

--- cmd/feedback.go
+++ cmd/feedback.go
@@ -18,12 +18,15 @@ func NewFeedbackCommand() *cobra.Command {
 
 			l.Debug().Msgf("Feedback command called.")
 
-			feedbackMessage := `Thank you for participating in early adoption of the refreshed Ping Identity universal CLI.
+			feedbackMessage := `Thank you for participating in early adoption of the refreshed Ping Identity universal CLI!
 
-We appreciate your feedback and information regarding your expirience with the CLI.
+We appreciate your feedback and suggestions for improvement regarding your experiences with the CLI.
 
-Please visit the following URL in your browser and let us know of any feedback or issues related
-to the tool:
+Please visit the following URL in your browser to fill out a short, anonymous survey that will help guide our development efforts and improve the CLI for all users:
+
+	https://forms.gle/xLz6ao4Ts86Zn2yt9
+
+If you encounter any bugs while using the tool, please open an issue on the project's GitHub repository's issue tracker:
 
 	https://github.com/pingidentity/pingctl/issues/new
 

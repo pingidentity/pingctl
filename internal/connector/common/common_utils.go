cffaff721a385f876712908a311d6e0d8c2b97c5
erikostien-pingidentity <69643860+erikostien-pingidentity@users.noreply.github.com>
2024-04-03 11:05:52 -0600

    PDI-1642: Update PingCTL with Internal Feedback (#108)
    - Fix export directory check to be in export.go instead
    of WriteFiles(). This avoids a bug where an initially
    empty export directory fails on the second call to
    WriteFiles if multiple services are exported.

--- internal/connector/common/common_utils.go
+++ internal/connector/common/common_utils.go
@@ -13,20 +13,6 @@ import (
 func WriteFiles(exportableResources []connector.ExportableResource, format, outputDir string, service string, overwriteExport bool) error {
 	l := logger.Get()
 
-	// Check if the output directory is empty
-	// If not, default behavior is to exit and not overwrite.
-	// This can be changed with the --overwrite export parameter
-	if !overwriteExport {
-		dirEntries, err := os.ReadDir(outputDir)
-		if err != nil {
-			return fmt.Errorf("failed to read export directory %q. err: %s", outputDir, err.Error())
-		}
-
-		if len(dirEntries) > 0 {
-			return fmt.Errorf("export directory %q is not empty. Use --overwrite to overwrite existing export data", outputDir)
-		}
-	}
-
 	// Make subdirectory for exported service if there are resources to export
 	if len(exportableResources) > 0 {
 		err := os.MkdirAll(filepath.Join(outputDir, service), os.ModePerm)
